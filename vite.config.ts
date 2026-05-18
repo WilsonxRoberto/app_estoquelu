66599e0df3a1b0a5918c470f692e570f002f85ec
˜wilsonxdrone <w178721@hotmail.com>
2026-05-17 21:31:26 -0300

    Update base path to app_estoquelu

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/app_estoque/',
+  base: '/app_estoquelu/',
   plugins: [
     react(),
     tailwindcss(),
